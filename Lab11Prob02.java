eea3ba55c521c72b0453d125053383a06e17be13
RJenks962 <rj09543@georgiasouthern.edu>
2024-11-22 16:42:49 -0500

    Update Lab11Prob02.java
    dddddd

--- Lab11Prob02.java
+++ Lab11Prob02.java
@@ -5,9 +5,9 @@ import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 
-public class Lab11Prob01 { 
+public class Lab11Prob02 { 
 	public static void main(String args[]) {
-		ArrayList<Person> personArrayList = new ArrayList<person>();
+		ArrayList<Person> personArrayList = new ArrayList<Person>();
 		try (
 			DataInputStream input = new DataInputStream(new FileInputStream("src/people.dat"));
 			DataOutputStream output = new DataOutputStream(new FileOutputStream("src/people-copy.dat"));
@@ -17,13 +17,13 @@ public class Lab11Prob01 {
 				output.writeInt(age);
 				String name = input.readUTF();
 				output.writeUTF(name);
-		        	String Address = input.readUTF();
+		        String Address = input.readUTF();
 				output.writeUTF(Address);
 				int zipCode = input.readInt();
 				output.writeInt(zipCode);
 				double salary = input.readDouble();
 				output.writeDouble(salary);
-				System.out.printf("%d %s %s %d %.2f%n", age, name, Address, zipCode, salary);
+				//System.out.printf("%d %s %s %d %.2f%n", age, name, Address, zipCode, salary);
 				personArrayList.add(new Person(age, name, Address, zipCode, salary));
 			}
 		} catch (EOFException e) {
@@ -31,7 +31,107 @@ public class Lab11Prob01 {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		personArrayList.sort(salary);
+		
+		try (ObjectOutputStream outdata = new ObjectOutputStream(new FileOutputStream("src/people-salary-sorted.dat"));
+				){
+			java.util.Collections.sort(personArrayList);
+			for (int i = 0; i < personArrayList.size(); i++) {
+				outdata.writeObject(personArrayList.get(i).toString());
+				System.out.println(personArrayList.get(i).toString());
+			}
+		} catch (EOFException e) {
+			System.out.print("");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+			
+		
+	}
+}
+class Person implements Comparable<Person> {
+	
+	private int age;
+	private String name;
+	private String address;
+	private int zipCode;
+	private double salary;
+	
+	//default constructor
+	public Person() {
+		setAge(20);
+		setName("John Doe");
+		setAddress("123 Main St");
+		setZipCode(12345);
+		setSalary(63000.3);
+	}
+	//convenience constructor
+	public Person(int age, String name, String address, int zipCode, double salary) {
+		setAge(age);
+		setName(name);
+		setAddress(address);
+		setZipCode(zipCode);
+		setSalary(salary);
+		
+	}
+
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public int getZipCode() {
+		return zipCode;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+
+		
+	public String toString() {
+		return String.format("%d %s %s %d %.2f", age, name, address, zipCode, salary);
+		
+	}
+	
+	@Override
+	public int compareTo(Person o) {
+		if (this.salary == o.salary) {
+			return 0;
+		} else {
+			if(this.salary > o.salary) {
+			return -1;
+		} else {
+			return 1;
+			}
+		}
 	}
 }
 
