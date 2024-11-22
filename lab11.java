cfc1074b17a2481913462670b566053f87c16752
RJenks962 <rj09543@georgiasouthern.edu>
2024-11-22 15:36:53 -0500

    Update lab11.java
    sfgdcb

--- lab11.java
+++ lab11.java
@@ -3,34 +3,30 @@ import java.io.EOFException;
 import java.io.FileOutputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.File;
-import java.util.Scanner;
 
-public class lab11 {
-	try (
-		DataInputStream input = new DataInputStream(new FileInputStream("src/people.dat"));
-		DataOutputStream output = new DataOutputStream(new FileOutputStream("src/people-copy.dat"));
+public class Lab11Prob01 { 
+	public static void main(String args[]) {
+		try (
+			DataInputStream input = new DataInputStream(new FileInputStream("src/people.dat"));
+			DataOutputStream output = new DataOutputStream(new FileOutputStream("src/people-copy.dat"));
 			) {
 			while(true) {
 				int age = input.readInt();
 				output.writeInt(age);
-				System.out.println(age);
 				String name = input.readUTF();
 				output.writeUTF(name);
-				System.out.println(name);
-				String Address = input.readUTF();
+		        String Address = input.readUTF();
 				output.writeUTF(Address);
-				System.out.println(Address);
 				int zipCode = input.readInt();
 				output.writeInt(zipCode);
-				System.out.println(zipCode);
 				double salary = input.readDouble();
 				output.writeDouble(salary);
-				System.out.println(salary);
+				System.out.printf("%d %s %s %d %.2f%n", age, name, Address, zipCode, salary);
 			}
 		} catch (EOFException e) {
-			System.out.println("End of File exception");
+			System.out.print("");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
 }
